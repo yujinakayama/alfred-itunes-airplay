2e092103ca67ac1a4a4d62a9c695b78c5bab88ea
Yuji Nakayama <nkymyj@gmail.com>
2015-02-11 20:56:17 +0900

    Initialize itunes-airplay Xcode project

---
+++ itunes-airplay/itunes-airplay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  itunes-airplay
+//
+//  Created by Yuji Nakayama on 2/11/15.
+//  Copyright (c) 2015 Yuji Nakayama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
