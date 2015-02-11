bd4594c2f51c609f38b8fcfb9c91150af165fd26
Yuji Nakayama <nkymyj@gmail.com>
2015-02-11 21:12:11 +0900

    Use the new @import statement

--- itunes-airplay/itunes-airplay/main.m
+++ itunes-airplay/itunes-airplay/main.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Yuji Nakayama. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
